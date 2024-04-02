0be41c0821bd52f3358db56e55ac4ca29f1a3f1f
julioclavijo <julioclavijo@outlook.com>
2024-04-02 07:58:59 -0500

    creacion página principal

--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const app  = express();
 const server = require('http').createServer(app);
 var io = require("socket.io")(server);
 
+app.use(express.static('client'));
+
 app.get('/hola-mundo', function(req, res){
     res.status(200).send('Hola Mundo!');
 });
 
+io.on('conection', function(socket){
+  console.log('el cliente con ip se ha conectado '+socket.handshake.address)
+});
 server.listen(8081, () => { console.log('Listening on port 8081') });
